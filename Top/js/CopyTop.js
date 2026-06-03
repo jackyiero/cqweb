93a84d002e1ebcaf0a57600167ab99225ff42e5c
jackyiero <74963594+jackyiero@users.noreply.github.com>
2026-06-03 21:50:10 +0800

    Add files via upload

---
+++ Top/js/CopyTop.js
@@ -0,0 +1,18 @@
+﻿document.write('<link rel="stylesheet" type="text/css" href="'+CopyRightcss+'">');
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('0 6=\'4://8.9-a.1/b/\';0 5=\'3/7.2\';0 g=\'3/h.2\';0 i=\'f\';0 c=\'4://d.e.1\';',19,19,'var|com|png|images|https|hao123|logoPath|uz16bq|static|sdg|china|copyright|hao123http|www|77boss|传奇高端网站设计制作|hao123logo|uz16logo|logotit'.split('|'),0,{}))
+
+
+var _sdgCopyrightLink = '#';
+var _sdgCopyrightHtml = '';
+_sdgCopyrightHtml +=''
+
++'' + '</div>' + '<div id="Version" >' + '<div id="Header">' + '<img src="Top/images/bg_01.jpg" width="1920">' + '<img src="Top/images/bg_02.jpg" width="1920">' + '<img src="Top/images/bg_03.jpg" width="1920">' + '<img src="Top/images/bg_04.jpg" width="1920">' + '<img src="Top/images/bg_05.jpg" width="1920">' + '</div>' + '' + '<div id="Flash">' + '<object width="100%" height="100%" codebase="/download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">'
++'<param value="Top/flash/banner.mp4" name="movie">'
++'<param value="transparent" name="wmode">'
++' <param value="true" name="loop">'
++' <param value="high" name="quality">'
++' <video src="Top/flash/banner.mp4" class="video-bg" autoplay loop muted></video>'
++'</object>' + '</div>' + '</div>'
+
+document.write(_sdgCopyrightHtml);
